04d6ad9deef9b6f982e15d42680b51a55f2f9572
Anirudh S <anisiva2005@gmail.com>
2025-07-05 17:07:33 +0530

    modified:   src/services/api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://mandala-nxgo.onrender.com';
+const API_BASE_URL = 'https://mandala-nxgo.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
